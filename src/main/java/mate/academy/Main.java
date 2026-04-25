67851a1cd5b75d516b9701523087cef2ede10bdc
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2026-04-25 19:21:20 +0200

    Task completed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.impl.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie fastAndFurious = new Movie();
+        fastAndFurious.setTitle("Fast and Furious");
+        fastAndFurious.setDescription("Cars, family, and action");
+
+        movieService.add(fastAndFurious);
+        System.out.println("Movie added: " + fastAndFurious);
+
+        Movie retrievedMovie = movieService.get(fastAndFurious.getId());
+        System.out.println("Movie retrieved: " + retrievedMovie);
     }
 }
